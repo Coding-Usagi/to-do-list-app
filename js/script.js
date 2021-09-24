fb41c907d699ffdefcbc9338d7631e2f2179a11e
Nioaes <whitney.a12221@gmail.com>
2021-09-24 19:25:41 -0400

    Cross referenced the example provided to fix errors, however the actual to do list does not function

--- js/script.js
+++ js/script.js
@@ -1,36 +1,35 @@
 function newItem(){
 
   //Adding a new item to the list of items
-  let li = $('.selectedElement').append('<li>item</li>')
+  let li = $('<li></li>')
   let inputValue = $('#input').val();
-  let text = $('.text').append('inputValue');
   li.append(li);
 
   if (inputValue === ''){
     alert("You must write something!");
   } else {
-    let list = $('.#list');
-    list.append(li);
+    $('#list').append(li);
 
     //Crossing out an item from the list of items
     function crossOut() {
-      li.classList.toggle("strike");
+      li.toggleClass("strike");
     }
 
-    $('li').on("dbclick", function (crossOut){
+    li.on("dbclick", function (crossOut){
+      li.toggleClass("strike")
     });
 
     //Adding the delete button "X"
-    let crossOutButton = $('.selectedElement').append("crossOutButton");
-    crossOutButton.append($('.text).append("X");
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
     li.append(crossOutButton);
 
-    $('crossOutButton').on("click", function (deleteListItem);
-
-    //Adding CLASS DELETE (DISPLAY: NONE) from the css:
+    crossOutButton.on("click", deleteListItem);
+      //Adding CLASS DELETE (DISPLAY: NONE) from the css:
     function deleteListItem(){
-      li.classList.add("delete")
+      li.addClass("delete")
     }
 
     $('#list').sortable();
+
   }
